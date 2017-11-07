e22cd174497607bc7b39c1ed4e0085321fa84c2c
Nathan N <Nitharios@users.noreply.github.com>
2017-11-07 11:04:25 -1000

    Backend for items working

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const users = require('./users');
 const items = require('./items');
 
-
 const router = express.Router();
 
 router.use('/users', users);
-router.use('items', items);
-
+router.use('/items', items);
 
 module.exports = router;
