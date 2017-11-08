e86ed500115c87f95a6b30cc967fd6bea359edd8
Justin Lee <leejwg.3@gmail.com>
2017-11-07 16:53:34 -1000

    home view basic

--- src/containers/HomeView/index.js
+++ src/containers/HomeView/index.js
@@ -1,5 +1,26 @@
-// import React, { Component } from 'react';
-// import Item from '../../components/Item';
+import React, { Component } from 'react';
+
 // import { connect } from 'react-redux';
 // import { getCards } from '../../actions/cards';
 
+class Home extends Component{
+  constructor(props){
+    super(props);
+  }
+}
+
+
+
+
+render(){
+  return(
+    <div className="home">
+      <div className="welcome-message">
+        <p>Welcome</p>
+      </div>
+      <div className="container"></div>
+    </div>
+    );
+}
+
+export default Home;
