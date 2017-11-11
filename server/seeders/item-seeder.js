77f127f7c3954c01ca419e3197d96ad44ce69e75
Justin Lee <leejwg.3@gmail.com>
2017-11-11 09:37:53 -1000

    starting new db

--- server/seeders/item-seeder.js
+++ server/seeders/item-seeder.js
@@ -1,28 +1,28 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('items', [
-    {
-      category: 'Vehicles',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      category: 'Appliances',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      category: 'Computers',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      category: 'Furniture',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
-},
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     return queryInterface.bulkInsert('items', [
+//     {
+//       category: 'Vehicles',
+//       createdAt: new Date(),
+//       updatedAt: new Date()
+//     },{
+//       category: 'Appliances',
+//       createdAt: new Date(),
+//       updatedAt: new Date()
+//     },{
+//       category: 'Computers',
+//       createdAt: new Date(),
+//       updatedAt: new Date()
+//     },{
+//       category: 'Furniture',
+//       createdAt: new Date(),
+//       updatedAt: new Date()
+//     }], {});
+// },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('items', null, {});
-  }
-};
+//   down: (queryInterface, Sequelize) => {
+//     return queryInterface.bulkDelete('items', null, {});
+//   }
+// };
