f9ed0226b0e1c39a6b4617fd8dd3848cd2153fd7
Justin Lee <leejwg.3@gmail.com>
2017-11-11 11:23:18 -1000

    commit before implementing faker

--- server/seeders/item-seeder.js
+++ server/seeders/item-seeder.js
@@ -4,176 +4,193 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('items', [
        {
-      item: 'kawasaki remote drone',
+      items: 'kawasaki remote drone',
+      name: 'someting',
+      description: '',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'cup',
+      items: 'cup',
+      description:'',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'mac',
+      items: 'mac',
+      name: 'someting',
+      description: '',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
     
-      item: 'honda cbr-600',
+      items: 'honda cbr-600',
+      name: 'someting',
+      description: '',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'designer beer glass',
+      items: 'designer beer glass',
+      name: 'someting',
+      description: '', 
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'phone',
+      items: 'phone',
+      name: 'someting',
+      description: '',  
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'tobbaco pipe',
+      items: '',
+      name: 'someting',
+      description: '', 
       createdAt: new Date(),
       updatedAt: new Date()
     },
      {
-      item: 'bmw m3',
+      items: 'bmw m3',
+      name: 'someting',
+      description: 'its something',  
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'tv',
+      items: 'tv',
+      name: 'someting',
+      description: 'hehehohoha', 
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'doom orginal',
+      items: 'doom orginal',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
     
-      item: '1997 dodge viper',
+      items: '1997 dodge viper',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'projector',
+      items: 'projector',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'xbox',
+      items: 'xbox',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'lazy boy chair',
+      items: 'lazy boy chair',
       createdAt: new Date(),
       updatedAt: new Date()
     },
      {
-      item: 'tractor',
+      items: 'tractor',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'mug for big coffee',
+      items: 'mug for big coffee',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'ps4',
+      items: 'ps4',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
     
-      item: '57 chevy truck',
+      items: '57 chevy truck',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'hunting rifle',
+      items: 'hunting rifle',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'Gaming computer',
+      items: 'Gaming computer',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 't-shirt',
+      items: 't-shirt',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: '1910 model - T',
+      items: '1910 model - T',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'cup',
+      items: 'cup',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'gameboy',
+      items: 'gameboy',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: '67 shelby mustang',
+      items: '67 shelby mustang',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'cup',
+      items: 'cup',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'alarm clock',
+      items: 'alarm clock',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'coffee table',
+      items: 'coffee table',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'toyota supra',
+      items: 'toyota supra',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'gym bag',
+      items: 'gym bag',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'cd player vintage',
+      items: 'cd player vintage',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'honda civic',
+      items: 'honda civic',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'table',
+      items: 'table',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'drone',
+      items: 'drone',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'couch',
+      items: 'couch',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'honda civic',
+      items: 'honda civic',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'cup',
+      items: 'cup',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'dslr 70d',
+      items: 'dslr 70d',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'knitty truck',
+      items: 'knitty truck',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'cup',
+      items: 'cup',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'samsung galaxy',
+      items: 'samsung galaxy',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      item: 'bed',
+      items: 'bed',
       createdAt: new Date(),
       updatedAt: new Date()
     }
