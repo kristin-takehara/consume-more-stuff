e2a6380858aac8d2fa9ad98b82e8d96bd86db3d0
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-11-13 16:12:29 -1000

    Sold & Unsold items separated into separate divs

--- src/components/user.components.js
+++ src/components/user.components.js
@@ -7,29 +7,43 @@ const UserItem = ({ singleUser }) => {
   return (
     <div className="single-user">
       <h1>Welcome, { singleUser.username }</h1>
-      <div className="unsold">
       { singleUser.Items.map((itemDetails) => {
         const url = /^http/;
         let imageUrl = itemDetails.imageUrl;
          // if imageUrl is not an http link AND this is a request from the single item view
         if (!url.test(imageUrl)) { imageUrl = '/' + imageUrl; }
-        return(
-        <div>
-          <Link to={`/items/${itemDetails.id}`}>
-            <h3>{ itemDetails.name }</h3>
-            <img className="uploaded-img" src={ imageUrl } alt="image not found" />
-          </Link>
-          <div>Price: ${ itemDetails.price } </div>
-          <div>Description: { itemDetails.description } </div>
-          <div>
-            <h3>Posted:</h3> <FormattedRelative value={ itemDetails.createdAt } />
+        if ( itemDetails.is_sold === 1 ){
+          return(
+          <div className="unsold">
+            <Link to={`/items/${itemDetails.id}`}>
+              <h3>{ itemDetails.name }</h3>
+              <img className="uploaded-img" src={ imageUrl } alt="image not found" />
+            </Link>
+            <div>Price: ${ itemDetails.price } </div>
+            <div>Description: { itemDetails.description } </div>
+            <div>
+              <h3>Posted:</h3> <FormattedRelative value={ itemDetails.createdAt } />
+            </div>
           </div>
-        </div>
-        );
+          );
+        } else {
+            return(
+            <div className="sold">
+              <Link to={`/items/${itemDetails.id}`}>
+                <h3>{ itemDetails.name }</h3>
+                <img className="uploaded-img" src={ imageUrl } alt="image not found" />
+              </Link>
+              <div>Price: ${ itemDetails.price } </div>
+              <div>Description: { itemDetails.description } </div>
+              <div>
+                <h3>Posted:</h3> <FormattedRelative value={ itemDetails.createdAt } />
+              </div>
+            </div>
+            );
+          }
         })
       }
       </div>
-    </div>
   );
 }
 
