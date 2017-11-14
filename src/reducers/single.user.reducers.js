adfc0b42c276a0cf1713870f40cc406916e74b08
Nathan N <Nitharios@users.noreply.github.com>
2017-11-14 13:51:43 -1000

    Reorganizing single.user.reducer

--- src/reducers/single.user.reducers.js
+++ src/reducers/single.user.reducers.js
@@ -1,4 +1,8 @@
 import { LOAD_SINGLE_USER } from '../actions/users.actions';
+import { REGISTER_USER,
+         LOGIN_USER,
+         LOGOUT_USER
+       } from '../actions/auth.actions';
 
 const initialState = { Items : [] };
 
@@ -6,6 +10,25 @@ const singleUserList = (state = initialState, action) => {
   switch (action.type){
     case LOAD_SINGLE_USER:
       return Object.assign({}, state, action.user);
+
+    case REGISTER_USER:
+      return Object.assign({}, state, initialState);
+
+    case LOGIN_USER:
+      const userDetails = action.userDetails;
+
+      localStorage.setItem('loggedin', true);
+      localStorage.setItem('userId', userDetails.id);
+      localStorage.setItem('username', userDetails.username);
+      localStorage.setItem('role', userDetails.role);
+
+      return Object.assign({}, state, action.userDetails);
+
+    case LOGOUT_USER:
+      localStorage.clear();
+
+      return Object.assign({}, state, initialState);
+     
     default:
       return state;
   }
