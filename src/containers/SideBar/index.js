438c5dd2ca722a4e7d43d2cf9047b03ca7085352
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-11-14 17:02:11 -1000

    side bars UP

--- src/containers/SideBar/index.js
+++ src/containers/SideBar/index.js
@@ -18,14 +18,37 @@ class SideBar extends Component {
   }
 
   render() {
+    if(localStorage.username) {
     return(
       <Menu>
           <a id="home" className="menu-item" href="/">Home</a>
+          <p>Categories</p>
+          <a id="appliances" className="menu-item" href="/appliances">Appliances</a>
+          <a id="computers" className="menu-item" href="/computers">Computers</a>
+          <a id="furniture" className="menu-item" href="/furniture">Furniture</a>
+          <a id="vehicles" className="menu-item" href="/vehicles">Vehicles</a>
+          <br/>
           <a id="about" className="menu-item" href="/about">About</a>
+          <a id="settings" className="menu-item" href="/settings">Settings</a>
           <a id="contact" className="menu-item" href="/contact">Contact</a>
-          <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
       </Menu>
-    );
+    )
+    }else{
+      return(
+        <Menu>
+          <a id="home" className="menu-item" href="/">Home</a>
+          <p>Categories</p>
+          <a id="appliances" className="menu-item" href="/appliances">Appliances</a>
+          <a id="computers" className="menu-item" href="/computers">Computers</a>
+          <a id="furniture" className="menu-item" href="/furniture">Furniture</a>
+          <a id="vehicles" className="menu-item" href="/vehicles">Vehicles</a>
+          <br/>
+          <a id="register" className="menu-item" href="/Register">Sign Up</a>
+          <a id="login" className="menu-item" href="/Login">Login</a>
+          <a id="contact" className="menu-item" href="/contact">Contact</a>
+        </Menu>
+      )
+    }
   }
 
 }
