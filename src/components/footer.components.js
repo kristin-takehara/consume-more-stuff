f8471c58b0c2a6379a77e6cdd65077fb0bad5b00
Nathan N <Nitharios@users.noreply.github.com>
2017-11-14 23:02:32 -1000

    Adjusted footer settings

--- src/components/footer.components.js
+++ src/components/footer.components.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
 
   return (
-    <div className="footer">
+    <div id="footer">
       <img src="/assets/plushy.png" alt="plushy"/>
       <br/>
       <div className="footer-details">
