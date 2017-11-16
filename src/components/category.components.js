98c865e430b15d94d656bbf73962179b5c67a7cd
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-11-15 16:29:13 -1000

    Vehicle components began

---
+++ src/components/category.components.js
@@ -0,0 +1,38 @@
+// component-container, a redux style dummy container
+import React from 'react';
+import Item from'./item.components';
+import { Link } from 'react-router-dom';
+
+const VehicleList = ({ items, categoryId, categoryName }) => {
+  return (
+    <div>
+      <div className="category-header">
+         <Link to={`/${categoryName}`}>{ categoryName }</Link>
+      </div>
+
+      <div className="vehicle-list" id={categoryName}>
+       { items
+        .filter(item => {
+          return !item.deletedAt;
+        })
+        .filter(item => {
+          return item.is_sold === 1;
+        })
+        .filter(item => {
+          return item.category_id === categoryId;
+        })
+        .map((item, idx) => {
+          //renders X amount of items
+          return (
+            <Item
+            singleItem={ item }
+            key={ idx } />
+          );
+        })
+      }
+      </div>
+    </div>
+  );
+}
+
+export default VehicleList;
